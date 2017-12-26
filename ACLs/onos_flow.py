6d22490a701682b658af7f0df74beb5cb4411f5a
Shashank6669 <sm4231@columbia.edu>
2017-12-25 23:59:57 -0500

    quarantine & static

--- ACLs/onos_flow.py
+++ ACLs/onos_flow.py
@@ -4,7 +4,8 @@ import argparse
 
 
 from flowtable import *
-
+#from quarantine_flow import *
+#from static_flow import *
 from requests.auth import HTTPBasicAuth
 from pprint import pprint
 
@@ -38,7 +39,6 @@ def ACL_Blacklist(IP,ID,mac,ip1, ip2):
 				fmac_dst = i['selector']['criteria'][1]['mac'].encode('ascii', 'ignore')
 				fmac_src = i['selector']['criteria'][2]['mac'].encode('ascii', 'ignore')
 				
-
 				if(fmac_dst.lower() == mac.lower() or fmac_src.lower() == mac.lower()):
 					delete_id.append(i['id'])
 					
@@ -61,8 +61,8 @@ def ACL_Blacklist(IP,ID,mac,ip1, ip2):
 	c = c + 1
 """
 def static_profile(IP,ID,mac):
-
-	reqURL = "http://"+IP+":8181/onos/v1/flows"
+	##stop onos-app-fwd in ONOS CLI
+	reqURL = "http://"+IP+":8181/onos/v1/flows/"
 	reqURL_dev = reqURL+ID
 	print(reqURL)
 	print(reqURL_dev)
@@ -70,11 +70,46 @@ def static_profile(IP,ID,mac):
 	fid, flow, rcode = GET(reqURL)
 
 	device = devices.find_one({'mac_address': mac})
+	acl = device['static_profile']
+
+	delete_id = []
+
+	# Delete all existing flows for the device
+		for i in flow['flows']:
+			if(len(i['selector']['criteria']) == 3):    
+				#print(i['selector']['criteria'])
+				
+				fmac_dst = i['selector']['criteria'][1]['mac'].encode('ascii', 'ignore')
+				fmac_src = i['selector']['criteria'][2]['mac'].encode('ascii', 'ignore')
+
+				if(fmac_dst.lower() == mac.lower() or fmac_src.lower() == mac.lower()):
+					delete_id.append(i['id'])
+					
+			#print(i['selector']['criteria'])
+		
+		#pprint(delete_id)
+
+		for d in delete_id:
+			response = DEL(reqURL_dev, d)
+			print(response)
 	
-	if(device['static_profile'].lower == 'quarantine'):
+	#Add flows according to MUD profile.
+	for f in range(3):
+		flow = S_flow(mac,ip1,ip2,f)
+		post_response=str(POST())
+
+
+
+def QUARANTINE(IP,ID,mac, q):
+	reqURL = "http://"+IP+":8181/onos/v1/flows/"
+	reqURL_dev = reqURL+ID
+
+	fid, flow, rcode = GET(reqURL)
+
+	if(q):
 
 		delete_id = []
-		# Delete all existing flows for the device
+		# Delete all existing flows for the device mac
 			for i in flow['flows']:
 				if(len(i['selector']['criteria']) == 3):    
 					#print(i['selector']['criteria'])
@@ -93,8 +128,13 @@ def static_profile(IP,ID,mac):
 			pprint(delete_id)
 		#Add flows to block communication to gateway.
 
+		for f in range(3):
+			flow = q_flow(mac, f)
+	    	post_response= str(POST(reqURL_dev, flow))
+	        print(post_response) 
 
 """
+
 def GET(URL):
 	response = requests.get(URL, auth=('onos', 'rocks'))
 	print response.status_code
---
+++ ACLs/static_flow.py
@@ -0,0 +1,99 @@
+def S_flow(mac, host1_ip, host2_ip, i):
+        return  [{
+                  "priority": 20,
+                  "timeout": 0,
+                  "tableId": 1,
+                  "isPermanent": 'true',
+                  "deviceId": "of:0000687f7429badf",
+                  "treatment": {
+                    "instructions": [
+                      {
+                        "type": "OUTPUT",
+                        "port": "21"
+                      }
+                    ]
+                  },
+                  "selector": {
+                    "criteria": [
+                      {
+                        "type": "IN_PORT",
+                        "port": 1
+                      },
+                      {
+                        "type": "ETH_SRC",
+                        "mac": mac
+                      },
+                      {
+                        "type": "ETH_DST",
+                        "mac": "60:E3:27:9B:44:BF"
+                      }
+                    ]
+                  }
+                },
+
+                {
+                          "priority": 20,
+                          "timeout": 0,
+                          "isPermanent": 'true',
+                          "deviceId": "of:0000687f7429badf",
+                          "treatment": {
+                            "instructions": [
+                              {
+                                "type": "OUTPUT",
+                                "port": "1"
+                              }
+                            ]
+                          },
+                          "selector": {
+                            "criteria": [
+                              {
+                                "type": "IN_PORT",
+                                "port": 21
+                              },
+                              {
+                                "type": "ETH_SRC",
+                                "mac": "60:E3:27:9B:44:BF"
+                              },
+                              {
+                                "type": "ETH_DST",
+                                "mac": mac
+                              }
+                            ]
+                          }
+                    },
+
+                    {
+                          "priority": 20,
+                          "timeout": 0,
+                          "isPermanent": 'true',
+                          "deviceId": "of:0000687f7429badf",
+                          "treatment": {
+                            "instructions": [
+                              {
+                                "type": "TABLE",
+                                "tableId": 1
+                              }
+                            ]
+                          },
+                          "selector": {
+                            "criteria": [
+                              {
+                                "type": "IN_PORT",
+                                "port": 1
+                              },
+                              {
+                                "type": "IPV4_SRC",
+                                "ip": host1_ip
+                              },
+                              {
+                                "type": "IPV4_DST",
+                                "ip": host2_ip
+                              },
+                              {
+                                "type": "ETH_TYPE",
+                                "ethType": "0x0800"
+                              }
+                            ]
+                          }
+                        }
+                        ][i]
